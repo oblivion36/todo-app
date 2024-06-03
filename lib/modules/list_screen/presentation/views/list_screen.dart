eb7fd57939bae4838ee21184f84d4ee93836067f
Rukshar Khanam <rukshar.khanam@centralogic.net>
2024-06-03 18:11:40 +0530

    half ui done

--- lib/modules/list_screen/presentation/views/list_screen.dart
+++ lib/modules/list_screen/presentation/views/list_screen.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:easy_date_timeline/easy_date_timeline.dart';
+import 'package:gap/gap.dart';
 
 class ListScreen extends StatelessWidget {
   const ListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: const Color(0xFFFBEFC9)),
+            child: const Center(
+              child: Icon(Icons.arrow_back),
+            ),
+          ),
+          actions: [
+            Container(
+              padding: const EdgeInsets.all(16),
+              height: 50,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: const Color(0xFFFBEFC9)),
+              child: const Center(
+                child: Row(
+                  children: [
+                    Icon(Icons.add),
+                    Text("Add Task"),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Gap(30),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: const Color(0xFFFBEFC9)),
+              child: EasyDateTimeLine(
+                initialDate: DateTime.now(),
+                onDateChange: (selectedDate) {},
+                activeColor: const Color(0xFFFDD868),
+                dayProps: const EasyDayProps(
+                  todayHighlightStyle: TodayHighlightStyle.none,
+                  todayHighlightColor: Color(0xFFFBEFC9),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
